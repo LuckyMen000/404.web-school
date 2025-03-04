401ecaa7205178078e6b5ceb36110d8b86c59bc5
DanilKossilov <kossilovdanil@outlook.com>
2025-03-04 18:52:10 +0500

    добавил оставшиеся страницы

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("header");
 
     function checkScroll() {
-        if (window.innerWidth > 768) { // Работает только на экранах больше 768px
+        if (window.innerWidth > 768) { 
             if (window.scrollY > 50) {
                 header.classList.add("scrolled");
             } else {
                 header.classList.remove("scrolled");
             }
         } else {
-            header.classList.remove("scrolled"); // Отключаем на мобильных
+            header.classList.remove("scrolled"); 
         }
     }
 
     window.addEventListener("scroll", checkScroll);
-    window.addEventListener("resize", checkScroll); // Проверка при изменении экрана
+    window.addEventListener("resize", checkScroll); 
 });
